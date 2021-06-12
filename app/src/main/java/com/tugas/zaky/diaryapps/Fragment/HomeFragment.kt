f23c59588026d9c6552a472eacad2e0009bd9183
farhan maulana siddiq <farhanmaulanasiddiq@gmail.com>
2021-06-13 04:18:01 +0700

    first commit

---
+++ app/src/main/java/com/tugas/zaky/diaryapps/Fragment/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.tugas.zaky.diaryapps.Fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tugas.zaky.diaryapps.R
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
